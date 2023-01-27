a9b8b3195be30428160cfd78027f1962464e29e3
cristianhdz <cristian@local.com>
2023-01-27 17:44:17 -0500

    fix: remove static variables from run file
    Removes the static variables from the
    run_subscription_manager.py file and put
    them into a separate configuration file.

--- 02_your_code/run_subs_manager.py
+++ 02_your_code/run_subs_manager.py
@@ -1,47 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Simple Python for connecting the CLI to the subscription manager library.
+Simple Python script to connect the CLI to the subscription manager library.
 
 Examples of usage:
+
 python run_subs_manager.py upgrade 1b2f7b83-7b4d-441d-a210-afaa970e5b76 premium
     output: 1b2f7b83-7b4d-441d-a210-afaa970e5b76 -- UPGRADED -- from free to premium
 
 python run_subs_manager.py downgrade 1b2f7b83-7b4d-441d-a210-afaa970e5b76 free
     output: 1b2f7b83-7b4d-441d-a210-afaa970e5b76 -- DOWNGRADED -- from premium to free
-
-Note: This file is only for testing purposes of the subscription manager library,
-and is not designed for being used in a production environment. 
 """
-
 import sys
 
+from settings_subs_manager import CUSTOMER_DATA_API_URL, SUBSCRIPTIONS
 from subscription_manager_base.subscription_manager.core import (
     DowngradeSubscription,
     UpgradeSubscription,
 )
 
 if __name__ == "__main__":
-    customer_data_api_url = "http://localhost:8010/api/v1/customerdata/"
-    subscriptions = {
-        "free": 1,
-        "basic": 2,
-        "premium": 3,
-    }
-
     if len(sys.argv) < 4:
         print("Usage: python script.py upgrade/downgrade uuid plan")
     else:
-        command = sys.argv[1]
-        uuid = sys.argv[2]
-        new_subscription = sys.argv[3]
+        COMMAND = sys.argv[1]
+        UUID = sys.argv[2]
+        NEW_SUBSCRIPTION = sys.argv[3]
 
-        if command == "upgrade":
+        if COMMAND == "upgrade":
             upgrade_manager = UpgradeSubscription(
-                uuid, new_subscription, customer_data_api_url, subscriptions
+                UUID, NEW_SUBSCRIPTION, CUSTOMER_DATA_API_URL, SUBSCRIPTIONS
             )
             print(upgrade_manager.upgrade())
-        if command == "downgrade":
+        if COMMAND == "downgrade":
             downgrade_manager = DowngradeSubscription(
-                uuid, new_subscription, customer_data_api_url, subscriptions
+                UUID, NEW_SUBSCRIPTION, CUSTOMER_DATA_API_URL, SUBSCRIPTIONS
             )
             print(downgrade_manager.downgrade())
---
+++ 02_your_code/settings_subs_manager.py
@@ -0,0 +1,10 @@
+"""
+Configuration to run the subscription manager library.
+"""
+CUSTOMER_DATA_API_URL = "http://localhost:8010/api/v1/customerdata/"
+
+SUBSCRIPTIONS = {
+    "free": 1,
+    "basic": 2,
+    "premium": 3,
+}
